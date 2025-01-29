b0de6b92401a1b9b080e622187bd02ab114f51f8
MaksimPro <maksimpronin1991@icloud.com>
2025-01-29 08:59:42 +0300

    fix: operation buttons after equal

--- js/main.js
+++ js/main.js
@@ -1,18 +1,33 @@
 const calcInputButton = document.querySelector(".calculator__key--clear")
+const operationsButtons = document.querySelectorAll(".calculator__key--operator");
 const keyButtons = document.querySelectorAll(".calculator__key");
 const calcInput = document.querySelector(".calculator__input");
 const backButton = document.querySelector(".calculator__key--backspace");
 const equalButton = document.querySelector(".calculator__key--equal");
 
+let isResultDisplayed = false;
+
 keyButtons.forEach(key => {
     key.addEventListener("click", () => {
+        if (isResultDisplayed && key.classList.contains("calculator__key--numbers")) {
+            calcInput.value = "";
+            isResultDisplayed = false;
+        }
         calcInput.value += key.textContent;
         console.log(calcInput.value)
     });
 });
 
+operationsButtons.forEach(operation => {
+    operation.addEventListener("click", () => {
+        isResultDisplayed = false
+    });
+});
+
+
 calcInputButton.addEventListener("click", () => {
     calcInput.value = "";
+    isResultDisplayed = false;
 });
 
 backButton.addEventListener("click", () => {
@@ -20,14 +35,14 @@ backButton.addEventListener("click", () => {
 });
 
 equalButton.addEventListener("click", () => {
-  
     try {
-        const result = eval(calcInput.value);
+        const result = Function('"use strict";return (' + calcInput.value + ')')();
         calcInput.value = result;
+        isResultDisplayed = true;
     } catch (error) {
         calcInput.value = result;
+        isResultDisplayed = true;
     }
- 
 });
 
 
